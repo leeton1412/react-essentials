7fa7be7f5b38c173488afd6b68f4b1b79d16f171
leeton1412 <leighton.griffith87@gmail.com>
2021-09-12 08:12:22 +0100

    added useState for loading, finished and fail

--- src/App.js
+++ src/App.js
@@ -5,21 +5,28 @@ import React, {useState, useEffect} from 'react';
 
 function App({login}) {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect (() => {
+    if(!login) return;
+    setLoading(true);
     fetch(`https://api.github.com/users/${login}`)
-    .then(Response => Response.json())
-    .then(setData);
-  }, [])
+      .then(Response => Response.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [login]);
 
-  if (data){
-    return <div>{JSON.stringify(data)}</div>
-  }
-  return (
-    <div>
-      No Data
-    </div>
-  )
+  if (loading) return <h1>Loading.....</h1>;
+  if(error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  if(!data) return null;
+
+  return <div>
+      <img alt={data.login} src={data.avatar_url}/>
+      <h1>{data.login}</h1>
+      <button>{data.repos_url}</button>
+      </div>
 }
 
 export default App;
